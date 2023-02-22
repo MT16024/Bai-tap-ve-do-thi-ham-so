dd2e29d577645037823dcf0466f12c63e98c474f
MT16024 <101939561+MT16024@users.noreply.github.com>
2023-02-22 22:18:00 +0700

    Create ham_so_phan_thuc.cpp

---
+++ ham_so_phan_thuc.cpp
@@ -0,0 +1,76 @@
+#include <GL/glut.h>
+#include <math.h>
+
+float a,b,c,d,e,min,max;
+int round(float x)
+{
+    if (x>0) return int (x+0.5);
+    else return int (x-0.5);
+}
+
+float f(float x) 
+{
+  return ((a*x + b)/ (c*x + d));
+}
+
+void initGL()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //R=1,G=1,B=1, anpha=1
+    glOrtho(-320,320,-240,240,-1,1);    
+}
+
+void Vedothi()    
+{
+    glColor3f(1.0, 0.0, 0.0);  // ==> Red
+      glBegin(GL_LINES);
+        glVertex2i(-320,0);
+        glVertex2i(320,0);
+        glVertex2i(0,-240);
+        glVertex2i(0,240);
+    glEnd();
+     int x1,y1, 
+         x2,y2; 
+     float x, dx, k; 
+    min=-320;
+    max=320;
+    
+     a=1;b=-1;c=1;d=-4;
+     k=(float)640/50; 
+     dx=0.1;    
+    glColor3f(0.0, 0.0, 1.0);  // ==> Blue
+     x=min;
+     x1=round(x*k);
+     y1=round(f(x)*k);
+      glBegin(GL_LINES);
+        glVertex2i(x1,y1);
+        glVertex2i(x1,y1);
+        while (x<max){
+          x=x+dx;
+          x2=round(x*k);
+          y2=round(f(x)*k);
+          glVertex2i(x1,y1);
+          glVertex2i(x2,y2);
+          x1=x2; y1=y2;      
+        }    
+    glEnd();
+}
+
+void mydisplay()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    Vedothi();
+    glViewport(0,0,640,480);
+    glFlush();
+}
+
+int main(int argc, char** argv){
+    int mode=GLUT_SINGLE | GLUT_RGB;
+    glutInitDisplayMode(mode);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Ve do thi ham so phan thuc");
+     
+    initGL();  
+    glutDisplayFunc(mydisplay);    
+    glutMainLoop();
+}
